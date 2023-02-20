9d86dc97e2009bec36946139bac4d223c1ed962e
Jason Watmore <cornflourblue@hotmail.com>
2023-02-20 18:32:38 +1100

    initial commit

---
+++ src/_components/Nav.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { authActions } from '_store';
+
+export { Nav };
+
+function Nav() {
+    const authUser = useSelector(x => x.auth.user);
+    const dispatch = useDispatch();
+    const logout = () => dispatch(authActions.logout());
+
+    // only show nav when logged in
+    if (!authUser) return null;
+    
+    return (
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <div className="navbar-nav">
+                <NavLink to="/" className="nav-item nav-link">Home</NavLink>
+                <button onClick={logout} className="btn btn-link nav-item nav-link">Logout</button>
+            </div>
+        </nav>
+    );
+}
